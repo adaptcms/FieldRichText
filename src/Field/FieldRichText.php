0324bee61fd37a0ddb898de28744c714ff69ac34
Charlie Page <charliepage88@gmail.com>
2020-07-05 13:13:35 -0400

    package field validation, misc ui & api fixes, is_required changes

--- src/Field/FieldRichText.php
+++ src/Field/FieldRichText.php
@@ -7,9 +7,28 @@ use Adaptcms\Fields\FieldType;
 class FieldRichText extends FieldType
 {
   /**
+  * @var array
+  */
+  public $defaultSettings = [
+    'options' => [
+      'is_sortable'        => false,
+      'is_searchable'      => true,
+      'is_required_create' => false,
+      'is_required_edit'   => false
+    ],
+    'action_rules' => [
+      'index'  => false,
+      'create' => true,
+      'edit'   => true,
+      'show'   => true,
+      'search' => true
+    ]
+  ];
+
+  /**
   * Migration Command
-  * When a module field, page field, or another variation is made
-  * from this field, you must supply a valid migration string in string format.
+  * When a package field is made from this field, you must supply a valid
+  * migration string in string format.
   * Here is an example, please note the use of `:columnName` and having the command
   * in single quotes so the CMS can execute the command via migrations:
   *
